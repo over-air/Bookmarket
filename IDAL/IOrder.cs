b49bc1aaccea81e09d9673d1c7103a9f517a6dd5
flyontheair <flyontheair@sina.cn>
2014-09-11 17:28:11 +0800

    同步
    到

---
+++ IDAL/IOrder.cs
@@ -0,0 +1,16 @@
+﻿using Models;
+
+namespace IDAL
+{
+    public interface IOrder
+    {
+        /// <summary>
+        /// 插入订单详情
+        /// </summary>
+        /// <param name="order"></param>
+        void Insert(Orderinfo order);
+
+        Orderinfo GetOrder(int orderinfoId);
+        
+    }
+}
