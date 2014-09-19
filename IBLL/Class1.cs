96255437bd1fe5186b9c742fae3b286ac31d265e
flyontheair <flyontheair@sina.cn>
2014-09-19 18:06:11 +0800

    同步

--- IBLL/Class1.cs
+++ IBLL/Class1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace IBLL
 {
-    public class Class1
+    public interface IClass1
     {
     }
 }
--- ViewModels/Class1.cs
+++ ViewModels/Class1.cs
@@ -8,5 +8,6 @@ namespace ViewModels
 {
     public class Class1
     {
+
     }
 }
