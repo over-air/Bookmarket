b3b07b86de4fe7f5ad31cbe667251112a6d67bfd
flyontheair <flyontheair@sina.cn>
2014-09-29 18:03:59 +0800

    修改商品详情model

--- ViewModels/ProDetails.cs
+++ ViewModels/ProDetails.cs
@@ -19,6 +19,21 @@ namespace ViewModels
         public Product Product { get; set; }
 
         /// <summary>
+        /// 新旧等级
+        /// </summary>
+        public Rank Rank { get; set; } 
+
+        /// <summary>
+        /// 销售数量
+        /// </summary>
+        public int Salesnum { get; set; }
+
+        /// <summary>
+        /// 左下栏商品推荐
+        /// </summary>
+        public IEnumerable<Product> ProductsRmd { get; set; }
+
+        /// <summary>
         /// 商品评价
         /// </summary>
         public IEnumerable<Review> Reviews { get; set; } 
