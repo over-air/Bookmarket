3b489781a767d5f2b223de0c6b518830df748117
flyontheair <flyontheair@sina.cn>
2014-09-30 16:36:47 +0800

    商品详情页面

---
+++ BookMarket/Controllers/ProdetailController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookMarket.Controllers
+{
+    public class ProdetailController : Controller
+    {
+        //
+        // GET: /Prodetail/
+        public ActionResult ProDetail()
+        {
+            return View();
+        }
+
+
+
+	}
+}
